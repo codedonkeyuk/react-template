bcc5aff799af5ff0bc7315f9fdc0e5607ae15bcb
Leigh <1142961+leighgordy@users.noreply.github.com>
2026-07-24 15:08:22 +0100

    Fixed production issue, was using wrong flag

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,13 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import CopyPlugin from "copy-webpack-plugin";
 import generateFavicons from "./plugins/generate-favicons.js";
 import injectServiceWorker from "./plugins/inject-service-worker.js";
 import siteConfig from "./assets/site-config.json" with { type: "json" };
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-const isProd = process.env.NODE_ENV === "production";
 
 const getMinifyOptions = (isProd) =>
   isProd && {
@@ -20,68 +19,73 @@ const getMinifyOptions = (isProd) =>
     minifyJS: true,
   };
 
-export default {
-  mode: isProd ? "production" : "development",
-  mode: "production",
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
+export default (env, argv) => {
+  const webpackMode = argv.mode || "production";
+  const isProd = webpackMode === "production";
+
+  return {
+    mode: webpackMode,
+    mode: "production",
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)x?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
         },
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".js", ".jsx", ".tsx", ".ts", ".json"],
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    clean: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./assets/index.html",
-      filename: "index.html",
-      title: siteConfig.appName,
-      metaDescription: siteConfig.metaDescription,
-      publicSiteAddress: siteConfig.publicSiteAddress,
-      isProduction: isProd,
-      minify: getMinifyOptions(isProd),
-    }),
-    new HtmlWebpackPlugin({
-      template: "./assets/404.html",
-      filename: "404.html",
-      title: `${siteConfig.appName} | Page Not Found `,
-      metaDescription: "Sorry, the page you are looking for does not exist.",
-      publicSiteAddress: siteConfig.publicSiteAddress,
-      isProduction: isProd,
-      minify: getMinifyOptions(isProd),
-      inject: false,
-    }),
-    ...(isProd
-      ? [
-          new CopyPlugin({
-            patterns: [{ from: "./assets/.htaccess", to: "." }],
-          }),
-          injectServiceWorker(__dirname),
-          generateFavicons(__dirname, siteConfig),
-        ]
-      : []),
-  ],
-  devServer: {
-    open: true,
-    static: {
-      directory: path.join(__dirname, "dist"),
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
     },
-    compress: true,
-    historyApiFallback: true,
-  },
+    resolve: {
+      extensions: [".js", ".jsx", ".tsx", ".ts", ".json"],
+    },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
+      clean: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./assets/index.html",
+        filename: "index.html",
+        title: siteConfig.appName,
+        metaDescription: siteConfig.metaDescription,
+        publicSiteAddress: siteConfig.publicSiteAddress,
+        isProduction: isProd,
+        minify: getMinifyOptions(isProd),
+      }),
+      new HtmlWebpackPlugin({
+        template: "./assets/404.html",
+        filename: "404.html",
+        title: `${siteConfig.appName} | Page Not Found `,
+        metaDescription: "Sorry, the page you are looking for does not exist.",
+        publicSiteAddress: siteConfig.publicSiteAddress,
+        isProduction: isProd,
+        minify: getMinifyOptions(isProd),
+        inject: false,
+      }),
+      ...(isProd
+        ? [
+            new CopyPlugin({
+              patterns: [{ from: "./assets/.htaccess", to: "." }],
+            }),
+            injectServiceWorker(__dirname),
+            generateFavicons(__dirname, siteConfig),
+          ]
+        : []),
+    ],
+    devServer: {
+      open: true,
+      static: {
+        directory: path.join(__dirname, "dist"),
+      },
+      compress: true,
+      historyApiFallback: true,
+    },
+  };
 };
